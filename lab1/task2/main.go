0a93ea9f2370146a3d2ce0d1c6adb63478fa99c0
Kentvyd <272630688+Kentvyd@users.noreply.github.com>
2026-09-09 23:38:45 +0400

    Task2 переменные разных типов

---
+++ lab1/task2/main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	var age int = 20
+	var price float64 = 73.59
+	var name string = "Владимир"
+	var isPrezident bool = true
+
+	fmt.Println("int:", age)
+	fmt.Println("float64:", price)
+	fmt.Println("string:", name)
+	fmt.Println("bool:", isPrezident)
+}
