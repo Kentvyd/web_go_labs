88c2765e757bd2e528a50f090f24ee165afa2633
Kentvyd <272630688+Kentvyd@users.noreply.github.com>
2026-09-15 12:36:48 +0400

    Task4 арифметика с двумя целыми числами

---
+++ lab1/task4/main.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a, b int = 15, 4
+
+	fmt.Println("сумма:", a+b)
+	fmt.Println("разность:", a-b)
+	fmt.Println("произведение:", a*b)
+	fmt.Println("частное:", a/b)
+	fmt.Println("остаток:", a%b)
+}
